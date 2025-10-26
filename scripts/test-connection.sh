161a08b5ce0386cfde6ecb9f3772da63e003b8ed
Kenny Williams <kwill@hey.com>
2025-10-26 09:51:50 -0700

    add shutdown-agents

--- scripts/test-connection.sh
+++ scripts/test-connection.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 echo "Testing database connection..."
-clojure -M --eval "(require '[datomic.client.api :as d]) (def client (d/client {:server-type :datomic-local :system \"mbrainz\"})) (d/connect client {:db-name \"mbrainz-1968-1973\"})"
+clojure -M --eval "(require '[datomic.client.api :as d]) (def client (d/client {:server-type :datomic-local :system \"mbrainz\"})) (d/connect client {:db-name \"mbrainz-1968-1973\"}) (shutdown-agents)"
 echo "Connection test successful"
